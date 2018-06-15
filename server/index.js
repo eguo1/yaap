2e7e0d7298389ab2334c1029a5a72090f285a5c7
Eric Guo <ericxguo@gmail.com>
2018-06-15 11:15:23 -0500

    trying to solve the CORS issue

--- server/index.js
+++ server/index.js
@@ -14,16 +14,16 @@ const createApp = () => {
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
 
-  app.use('/api', require('./api'))
-
-  app.use(express.static(path.join(__dirname, '..', 'public')))
-
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   })
+
+  app.use('/api', require('./api'))
+
+  app.use(express.static(path.join(__dirname, '..', 'public')))
 
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {

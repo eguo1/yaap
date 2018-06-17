9caf67b91ab4ed2085674dbf25d76ea3030e7411
Eric Guo <ericxguo@gmail.com>
2018-06-17 17:36:51 -0500

    added another test

--- client/store/event_data.spec.js
+++ client/store/event_data.spec.js
@@ -15,18 +15,41 @@ const fakeSingleResult = [{
   events: 1
 }]
 
+const fakeEventData = [{
+  id: 1,
+  type: 'click',
+  timeElapsed: 52
+}, {
+  id: 2,
+  type: 'click',
+  timeElapsed: 52
+}]
+
+const fakeResult = [{
+  seconds: 52,
+  events: 2
+}]
+
 describe('Event Data reducer', () => {
   it('should return the initial state', () => {
     expect(eventDataReducer([], 'not-a-valid-action')).to.deep.equal([])
   })
 
   describe('the UPDATE_EVENT_DATA action', () => {
-    it('should single new events', () => {
+    it('should handle new events', () => {
       const updateData = {
         type: UPDATE_EVENT_DATA,
         eventData: fakeSingleEventData
       }
       expect(eventDataReducer([], updateData)).to.deep.equal(fakeSingleResult)
+    })
+
+    it('should handle multiple new events', () => {
+      const updateData = {
+        type: UPDATE_EVENT_DATA,
+        eventData: fakeEventData
+      }
+      expect(eventDataReducer([], updateData)).to.deep.equal(fakeResult)
     })
   })
 })

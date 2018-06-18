bfdc6a7e6d829f298c083a7ff8feb0c1b10305e0
Eric Guo <ericxguo@gmail.com>
2018-06-17 20:32:26 -0500

    converted Visualization to a smart component

--- client/components/visualization.js
+++ client/components/visualization.js
@@ -4,12 +4,16 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchEventData } from '../store/client_events'
 
-const Visualization = () => (
-  <div>
-    <h1>Yet Another Analytics Platform</h1>
-    <hr />
-  </div>
-)
+class Visualization extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Yet Another Analytics Platform</h1>
+        <hr />
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = state => {
   return {
